68fd93bdf4420b4f804938129a3c609683381263
ZeFurtado <lukhasfurtado9@gmail.com>
2023-02-16 18:32:17 -0300

    Revisando classes

---
+++ 01_Classe/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _01_Classe 
+{
+    class Pessoa
+    {
+        string Nome;
+        string Sexo;
+        DateTime DataNascimento;
+
+
+        public string ResumoPessoa() 
+        {
+            string ResumoPessoa = $"Nome: {Nome}";
+
+            return ResumoPessoa;
+        }
+    }
+}
